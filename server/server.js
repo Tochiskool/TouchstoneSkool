c3d46141be66f59f3817db60438182f7564320b9
Tochiskool <mrchristian79@yahoo.com>
2020-11-13 04:33:27 +0100

    Script added to run project and Mongodb is connected to the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import devBundle from './devBundle';//commented out when in production mode
 import path from 'path';
 import template from './../template';
+import { MongoClient } from 'mongodb';
 
 const CURRENT_WORKING_DIR = process.cwd;
 const app = express();
@@ -20,4 +21,13 @@ app.listen(PORT, function onStart(err) {
         console.log(err)
     }
     console.info(`Server started on port %s. ${PORT}`)
+})
+
+// Database Connection URL
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+
+// Use connect method to connect to the server
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },(err, db)=>{
+  console.log("Connected successfully to mongodb server")
+  db.close()
 })
